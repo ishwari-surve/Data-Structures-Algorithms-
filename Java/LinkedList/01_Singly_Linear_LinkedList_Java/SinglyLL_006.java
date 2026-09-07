3ddde5e71dbc2f6ed1ee4a327cc0497cf8e017e7
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-07 17:38:55 +0530

    Added Java SinglyLL Program 006 for Traversal Using While Loop - Implemented node class with data and next fields - Created three node objects explicitly using 'new' keyword - Demonstrated manual linking: obj1.next = obj2 - Implemented traversal using while(temp != null) loop - Output data in insertion order: 11, 21, 51 - Showed Java syntax vs C differences - Foundation for function-based linked list implementations  Concepts: Object Creation, Manual Linking, Traversal, Java Syntax

--- Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_006.java
+++ Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_006.java
@@ -2,7 +2,7 @@
 ================================================================================
 Program Name    : Singly Linear Linked List - Traversal Using While Loop
 Program Number  : 006 
-Description     : This program demonstrates traversal of a Singly
+Description     : This program demonstrates traversal of a Singly 
                   Linear Linked List using a while loop.
                   Three node objects are created manually, linked
                   together using next references, and displayed by

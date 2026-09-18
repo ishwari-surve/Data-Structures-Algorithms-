12ec40e60476156086aeb21fcb966dd00c8a24f2
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-18 09:53:12 +0530

    Add Java  Program 008 : For Creating the Linked List Class

--- Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_008.java
+++ Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_008.java
@@ -3,7 +3,7 @@
 Program Name : Singly Linear Linked List - Linked List Class Initialization
 Program Number  : 008
 Description     : This program demonstrates how to create a
-                  separate SinglyLL class to represent a
+                  separate SinglyLL class to represent a 
                   Singly Linear Linked List in Java.
 
                   The constructor initializes the first

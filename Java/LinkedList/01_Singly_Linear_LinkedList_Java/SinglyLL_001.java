b29e56f41f6eb59394edc081fa8c0154b9e85b8d
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-22 23:51:39 +0530

    Added Program 001 : Demonstrating basic node creation

--- Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_001.java
+++ Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_001.java
@@ -1,7 +1,7 @@
 /*
-===============================================================================
+=============================================================================== 
 Program Name    : Singly Linear Linked List - Node Creation 
-Program Number  : 001
+Program Number  : 001 
 Description     : This program demonstrates the creation of a single node
                   for a Singly Linear Linked List in Java.
                   A node object is created dynamically using the 

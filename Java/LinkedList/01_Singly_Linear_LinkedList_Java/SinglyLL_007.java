615e669db996eaa5cbe1d68832a44a7d292e1d16
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-09-25 12:09:13 +0530

    Added Program 007 : For Parameterized Constructor

--- Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_007.java
+++ Java/LinkedList/01_Singly_Linear_LinkedList_Java/SinglyLL_007.java
@@ -1,7 +1,7 @@
 /*
 ================================================================================ 
 Program Name : Singly Linear Linked List - Parameterized Constructor
-Program Number  : 007 
+Program Number  : 007  
 Description     : This program demonstrates the use of a 
                   parameterized constructor to initialize
                   the data and next reference of a node 
